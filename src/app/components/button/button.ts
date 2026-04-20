2884df25e7c611d770cf92febb2388f5bb6b9e70
Laszlo Nemeth <nemeth.rzr@gmail.com>
2026-04-20 11:11:57 +0200

    feat button component @Input -> signal input

--- src/app/components/button/button.ts
+++ src/app/components/button/button.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, HostBinding } from '@angular/core';
+import { Component, Input, HostBinding, input, signal } from '@angular/core';
+
+type ButtonVariant = 'purple' | 'blue' | 'dark' | 'red' | 'transparent';
+type ButtonType = 'normal' | 'link';
+type ButtonIcon = 'caret' | 'none';
 
 @Component({
   selector: 'button[appButton], a[appButton]',
@@ -7,7 +11,7 @@ import { Component, Input, HostBinding } from '@angular/core';
   standalone: true,
   styleUrl: './button.css',
   template: `
-    @if (icon !== 'none') {
+    @if (icon() !== 'none') {
       <svg width="7" class="icon" height="10" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M6 9L2 5l4-4"
@@ -22,14 +26,15 @@ import { Component, Input, HostBinding } from '@angular/core';
   `,
 })
 export class ButtonComponent {
-  @Input() variant: 'purple' | 'blue' | 'dark' | 'red' | 'transparent' = 'transparent';
-  @Input() type: 'normal' | 'link' = 'normal';
-  @Input() icon: 'caret' | 'none' = 'none';
-  @Input() disabled: boolean = false;
+  variant = input<ButtonVariant>('transparent');
+  type = input<ButtonType>('normal');
+  icon = input<ButtonIcon>('none');
+
+  disabled = input<boolean>(false);
 
   @HostBinding('class')
   get classes() {
-    const variantClasses: Record<typeof this.variant, string> = {
+    const variantClasses: Record<ButtonVariant, string> = {
       purple: 'purple-button',
       blue: 'blue-button',
       dark: 'dark-button',
@@ -37,17 +42,17 @@ export class ButtonComponent {
       transparent: 'transparent-button',
     };
 
-    const typeClasses: Record<typeof this.type, string> = {
+    const typeClasses: Record<ButtonType, string> = {
       normal: 'base-button',
       link: 'link-button',
     };
 
     return [
-      typeClasses[this.type],
-      variantClasses[this.variant],
+      typeClasses[this.type()],
+      variantClasses[this.variant()],
 
-      this.icon !== 'none' ? 'has-icon' : '',
-      this.disabled ? 'disabled' : '',
+      this.icon() !== 'none' ? 'has-icon' : '',
+      this.disabled() ? 'disabled' : '',
     ]
       .filter(Boolean)
       .join(' ');
