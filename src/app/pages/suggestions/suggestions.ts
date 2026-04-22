01275df77bb6c502d0e3c4da3aa82c96f22b28c4
Laszlo Nemeth <nemeth.rzr@gmail.com>
2026-04-22 16:55:26 +0200

    chore missing file(suggestion component)

---
+++ src/app/pages/suggestions/suggestions.ts
@@ -0,0 +1,17 @@
+import { Component, inject } from '@angular/core';
+import { SideBar } from '../../layout/side-bar/side-bar';
+import { Header } from '../../layout/header/header';
+import { FeedbackStore } from '../../shared/feedback.store';
+import { Badge } from '../../components/badge/badge';
+import { ButtonComponent } from '../../components/button/button';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-suggestions',
+  imports: [SideBar, Header, Badge, ButtonComponent, RouterLink],
+  templateUrl: './suggestions.html',
+  styleUrl: './suggestions.css',
+})
+export class Suggestions {
+  store = inject(FeedbackStore);
+}
